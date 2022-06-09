fe826a58c7507c05c57b7a204f9c3c4204e83b1b
Jorge Felix <jorge30fm@gmail.com>
2022-06-09 19:04:04 -0400

    validation

--- app.js
+++ app.js
@@ -6,17 +6,46 @@ const promptUser = () => {
                 {
                         type: 'input',
                         name: 'name',
-                        message: 'What is your name?'
+                        message: 'What is your name?',
+                        validate: nameInput => {
+                                if (nameInput) {
+                                        return true;
+                                } else {
+                                        console.log('Please enter your name!')
+                                        return false
+                                }
+                        }
                 },
                 {
                         type: 'input',
                         name: 'github',
-                        message: 'Enter your GitHub Username'
+                        message: 'Enter your GitHub Username',
+                        validate: nameInput => {
+                                if (nameInput) {
+                                        return true;
+                                } else {
+                                        console.log('Please enter your GitHub username!')
+                                        return false
+                                }
+                        }
+                },
+                {
+                        type: 'confirm',
+                        name: 'confirmAbout',
+                        message: "Would you like to enter some information about yourself for an 'About' section?",
+                        default: true
                 },
                 {
                         type: 'input',
                         name: 'about',
-                        message: 'Provide some information about yourself:'
+                        message: 'Provide some information about yourself:',
+                        when: ({ confirmAbout }) => {
+                                if (confirmAbout) {
+                                        return true;
+                                } else {
+                                        return false;
+                                }
+                        }
                 }
         ]);
 };
@@ -34,12 +63,28 @@ const promptProject = portfolioData => {
                 {
                         type: 'input',
                         name: 'name',
-                        message: 'What is the name of your project?'
+                        message: 'What is the name of your project?',
+                        validate: nameInput => {
+                                if (nameInput) {
+                                        return true;
+                                } else {
+                                        console.log('Please enter the name of your project!')
+                                        return false
+                                }
+                        }
                 },
                 {
                         type: 'input',
                         name: 'description',
-                        message: 'Provide a description of the project (Required)'
+                        message: 'Provide a description of the project (Required)',
+                        validate: nameInput => {
+                                if (nameInput) {
+                                        return true;
+                                } else {
+                                        console.log('Please describe your project!')
+                                        return false
+                                }
+                        }
                 },
                 {
                         type: 'checkbox',
@@ -50,7 +95,15 @@ const promptProject = portfolioData => {
                 {
                         type: 'input',
                         name: 'link',
-                        message: 'Enter the GitHib link to your project. (Required)'
+                        message: 'Enter the GitHib link to your project. (Required)',
+                        validate: nameInput => {
+                                if (nameInput) {
+                                        return true;
+                                } else {
+                                        console.log('Please enter the GiHub link to your project!')
+                                        return false
+                                }
+                        }
                 },
                 {
                         type: 'confirm',
