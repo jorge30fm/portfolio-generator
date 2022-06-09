c5509ea5f7e7685aa4f50e43aba6f75e589b1e19
Jorge Felix <jorge30fm@gmail.com>
2022-06-09 15:47:52 -0400

    require inquirer

---
+++ app.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+inquirer
+        .prompt([
+                {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is your name?'
+                }
+        ])
+        .then(answers => console.log(answers));
+// const FileSystem = require('fs');
+// const generatePage =require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// FileSystem.writeFile('./index.html', pageHTML, err =>{
+//         if(err) throw err;
+//         console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
+
+
+
+
